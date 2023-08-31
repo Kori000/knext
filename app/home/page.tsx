a015e1870b5fa8bb1725b19accb9e057f52c1b8b
Kori <kexin@korix.top>
2023-08-31 15:54:22 +0800

    init

---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React, { memo, useEffect, useMemo, useState } from 'react'
+
+const page = () => {
+  throw new Error('nihao')
+
+  return <div>NI好</div>
+}
+
+export default page
